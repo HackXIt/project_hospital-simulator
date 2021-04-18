07350428f0f00e9d3a642240a5cffc3a66c44bc8
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-04-18 19:32:58 +0200

    Created initial project files during meeting in collaboration of team

--- src/main.c
+++ src/main.c
@@ -1,13 +1,29 @@
-//
-// on branch steve
-//
+/* MAIN.c
+ *   by Nikolaus Rieder, Stefan Bittgen, Eva Grabler
+ *
+ * Created:
+ *   4/18/2021, 7:26:45 PM
+ * Last edited:
+ *   4/18/2021, 7:30:21 PM
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Main execution of the project
+**/
+
+/*--- COMMON LIBRARIES ---*/
+
+/*--- CUSTOM LIBRARIES ---*/
+#include "hospital_structures.h"
+#include "persons.h"
+/*--- MACROS ---*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-
-
+int main()
+{
+    ListPersons_t activePersons = createListPersons();
     return 0;
-
 }
