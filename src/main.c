50f20bd4954e3abac8da9c9526631064dd364a90
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-04-20 19:05:47 +0200

    Updated main.c with examples and added source files Added persons.c & persons.h => used for code relating to patient-logic Added seat_rows.c & seat_rows.h => used for code relating to seat-logic

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
-//
-// on branch steve
-//
+/* MAIN.c
+ *   by Nikolaus Rieder, Stefan Bittgen, Eva Grabler
+ * Description:
+ *   Main execution of the project
+**/
+
+/*--- COMMON LIBRARIES ---*/
+
+/*--- CUSTOM LIBRARIES ---*/
+#include "hospital_structures.h"
+/*--- MACROS ---*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
-
+    // Possible example of Person-List-creation: ListPersons_t activePersons = createListPersons();
+    // ^ Will be executed 2 times to generate an active Persons-List and an inactive Persons-List
+    // Possible example of Row-List-creation: ListRows_t row1 = createListRow();
+    // ^ Will be executed 5 times to generate 5 rows.
     return 0;
-
 }
---
+++ src/seat_rows.c
@@ -0,0 +1,9 @@
+
+//Create Sitzreihe - Nick
+
+//Assign Sitzreihe (Zuweisen) - Nick
+
+//Select Sitzreihe (Auswahl des freien Platzes) - Nick
+
+//Free Sitzreihe - Nick
+
