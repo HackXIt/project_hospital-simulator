1cacc9251576d494758e576dbfe1d2eb5db2ed67
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-04-25 20:13:32 +0200

    Fixed "Not looping"-Error in seat logic

--- src/hospital_structures.h
+++ src/hospital_structures.h
@@ -3,43 +3,43 @@
 
 struct node_p
 {
-    struct Person *next;
-    struct Person *prev;
+	struct Person *next;
+	struct Person *prev;
 };
 struct node_s
 {
-    struct Seat *next;
-    struct Seat *prev;
+	struct Seat *next;
+	struct Seat *prev;
 };
 
 typedef struct Person
 {
-    unsigned short num;
-    char arrival; // Valid arrivals are: Z, R, (K is optional)
-    char *first_name;
-    char *last_name;
-    struct Person *neighbour[2]; // storage of left & right neighbour
-    struct Seat *seat;           // reference to seat, if any
-    struct node_p node;
+	unsigned short num;
+	char arrival; // Valid arrivals are: Z, R, (K is optional)
+	char *first_name;
+	char *last_name;
+	struct Person *neighbour[2]; // storage of left & right neighbour
+	struct Seat *seat;			 // reference to seat, if any
+	struct node_p node;
 } Person_t;
 
 typedef struct Seat
 {
-    struct Person *occupied; // if occupied != NULL then it is occupied
-    struct node_s node;
+	struct Person *occupied; // if occupied != NULL then it is occupied
+	struct node_s node;
 } Seat_t;
 
 typedef struct ListPersons
 {
-    unsigned short count;
-    struct Person *start;
-    struct Person *last;
+	unsigned short count;
+	struct Person *start;
+	struct Person *last;
 } ListPersons_t;
 
 typedef struct ListRows
 {
-    unsigned short count;
-    struct Seat *start;
-    struct Seat *last;
+	unsigned short count;
+	struct Seat *start;
+	struct Seat *last;
 } ListRows_t;
 #endif
