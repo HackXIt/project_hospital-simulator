d6f920db6a10f9d6147d80e1380d73485ea89609
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-04-25 19:07:13 +0200

    Added seat_row logic
    createSeat() - allocates memory for the structure Seat_t
    createRow() - allocates memory for the structure ListRows_t
    occupySeat() - links a seat with a person in the given row
    selectRow() - finds the row with the least persons and assigns the given person a seat
    freeRow() - free's the allocated memory for the structure ListRows_t (including seats)
    freeSeat() - free's the allocated memory for the structure Seat_t and re-links the list in that spot

--- src/seat_rows.c
+++ src/seat_rows.c
@@ -1,9 +1,130 @@
+/*--- COMMON LIBRARIES ---*/
+#include <stdio.h>
+#include <stdlib.h>
+/*--- CUSTOM LIBRARIES ---*/
+#include "hospital_structures.h"
+/*--- MACROS ---*/
+#define MAX_ROWS 5
+#define MAX_SEATS 5
 
-//Create Sitzreihe - Nick
+// Create Seat
+Seat_t *createSeat()
+{
+    Seat_t *newSeat = calloc(1, sizeof(Seat_t));
+    if (newSeat == NULL)
+    {
+        // TODO Memory-Error-Handling in createSeat();
+        return NULL;
+    }
+    return newSeat;
+}
 
-//Assign Sitzreihe (Zuweisen) - Nick
+// Create Sitzreihe - Nick
+ListRows_t *createRow()
+{
+    ListRows_t *newRow = calloc(1, sizeof(ListRows_t)); // Initializes values with
+    if (newRow == NULL)
+    {
+        // TODO Memory-Error-Handling in createRow();
+        return NULL;
+    }
+    newRow->count = 0;
+    newRow->start = createSeat();
+    newRow->last = newRow->start;
+    for (int i = 0; i < MAX_SEATS - 1; i++)
+    {
+        Seat_t *newSeat = createSeat();
+        newRow->last->node.next = newSeat;
+        newSeat->node.prev = newRow->last;
+        newRow->last = newSeat;
+    }
+    return newRow;
+}
+
+// Occupy a seat in the row with the given person
+int occupySeat(ListRows_t *row, Person_t *person)
+{
+    Seat_t *current = row->start;
+    // Checking if the row is full must be done before occupying, otherwise there will be SEG_FAULT
+    // FIXME forgot while loop, not iterating through list
+    if (current->occupied != NULL)
+    {
+        current = current->node.next;
+    }
+    else
+    {
+        // TODO Create logic where an empty seat between persons is preferred, if possible at all
+        // Current logic is "First come, first served"
+        current->occupied = person;
+        person->seat = current;
+        row->count++;
+        return EXIT_SUCCESS;
+    }
+    // This can never be reached, because if the row is fully occupied, it causes a SEG_FAULT
+    return EXIT_FAILURE;
+}
+
+// Assign Person to seat - Nick
+// int assignSeat(ListRows_t *row, Person_t *person)
+// {
+//     if (row->count < 5)
+//     {
+//         Seat_t *temp = occupySeat(person);
+//         if (temp == NULL)
+//         {
+//             fprintf(stderr, "Couldn't occupy seat.");
+//             return EXIT_FAILURE;
+//         }
+//         row->last->node.next = temp;
+//         temp->node.prev = row->last;
+//         row->last = temp;
+//         return EXIT_SUCCESS;
+//     }
+//     return EXIT_FAILURE;
+// }
 
 //Select Sitzreihe (Auswahl des freien Platzes) - Nick
+// Select the row with the lowest person count and occupy a seat in that row
+int selectRow(ListRows_t *rows[MAX_ROWS], Person_t *person)
+{
+    ListRows_t *selection = NULL;
+    int lowest = MAX_SEATS;
+    for (int i = 0; i < MAX_ROWS; i++)
+    {
+        if (lowest > rows[i]->count)
+        {
+            selection = rows[i];
+            lowest = rows[i]->count;
+        }
+    }
+    if (selection != NULL)
+    {
+        occupySeat(selection, person);
+        return EXIT_SUCCESS;
+    }
+    // TODO Error-Message when all rows are full
+    return EXIT_FAILURE;
+}
 
-//Free Sitzreihe - Nick
-
+// Free the whole row from memory - Nick
+int freeRow(ListRows_t *row)
+{
+    Seat_t *current = row->start; // Temporarily save current node
+    while (current != NULL)
+    {
+        row->start = current->node.next; // move head of list to next node
+        free(current);                   // free current node
+        current = row->start;            // move current node to head
+    }
+    free(row); // free list head
+    return EXIT_SUCCESS;
+}
+// Free a single seat in the list
+int freeSeat(Seat_t *seat)
+{
+    Seat_t *temp = seat->node.prev;               // Temporarily save previous seat
+    seat->node.prev->node.next = seat->node.next; // Link previous seat with next seat
+    seat->node.next->node.prev = temp;            // Link next seat with the previous, using the temporary pointer
+    free(seat);                                   // Free the now unlinked seat
+    return EXIT_SUCCESS;
+}
