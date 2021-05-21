b71db785391c265fd9e4b585db86d4c0c27670db
Eva Grabler <ic20b002@technikum-wien.at>
2021-05-21 21:33:36 +0200

    persons.h: Updated header file with new functions.

--- src/persons.h
+++ src/persons.h
@@ -8,8 +8,10 @@ ListPersons_t *createListPersons();
 Person_t * fillStructPerson(unsigned short num, char arrival, char *first_name, char *last_name);
 Person_t * fillStructPersonMan(ListPersons_t *list);
 int addPerson(ListPersons_t *list, Person_t *person);
-int movePerson(ListPersons_t *list);
+int movePerson(ListPersons_t *listActive, ListPersons_t *listCompleted);
+int appendPerson(Person_t *person, ListPersons_t *list);
 void freeListPersons(ListPersons_t *list);
+void printPerson(Person_t *person);
 void printListPersons(ListPersons_t *list);
 void exportListPersons(ListPersons_t *list);
 
