b313f9d17430b9586e297c7a46b765e50d20295d
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-05-26 19:52:30 +0200

    Fixed CMakeLists missing library and added first test for explanation

--- tests/person_tests.cxx
+++ tests/person_tests.cxx
@@ -2,6 +2,7 @@
 
 extern "C"
 {
+#include "hospital_structures.h"
 #include "persons.h"
 }
 
@@ -9,4 +10,22 @@ extern "C"
 TEST(person_unit_tests, DUMMY)
 {
 	EXPECT_EQ(1, 1);
+}
+
+//
+TEST(person_unit_tests, fillStructPerson_assert_static)
+{
+	Person_t ref_person = {
+		.num = 0,
+		.arrival = 'Z',
+		.first_name = "Peter",
+		.last_name = "Lustig",
+		.neighbour = {NULL, NULL},
+		.seat = NULL,
+		.node = {.next = NULL, .prev = NULL}};
+	Person_t *test_person = fillStructPerson(0, 'Z', "Peter", "Lustig");
+	EXPECT_EQ(ref_person.arrival, test_person->arrival);
+	EXPECT_STREQ(ref_person.first_name, test_person->first_name);
+	EXPECT_STREQ(ref_person.last_name, test_person->last_name);
+	// NULL-Pointer kann man auch vergleichen, lass ich aber hier kurz weg
 }
