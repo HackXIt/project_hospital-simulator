ea96d7918344a7e1c6716207765b80373d55e001
Stefan <stefan.bittgen@gmx.net>
2021-05-29 18:14:41 +0200

    Bug in seat_rows_tests.cxx : Undefined reference to Gtk functions

--- tests/seat_row_tests.cxx
+++ tests/seat_row_tests.cxx
@@ -1,25 +1,21 @@
 #include "gtest/gtest.h"
-#include "seat_rows.h"
+
+
 
 extern "C"
 {
 #include "seat_rows.h"
-#include "persons.h"
-}
+#include "hospital_structures.h" // -> Project-Datastructures
+#include "persons.h"			 // -> Functions for persons & personLists
 
-/*
-	Person_t ref_person = {
-		.arrival = 'Z',
-		.first_name = "Peter",
-		.last_name = "Lustig",
-		.neighbour = {NULL, NULL},
-		.seat = NULL,
-		.node = {.next = NULL, .prev = NULL}};
-	//Person_t *test_person = fillStructPerson('Z', "Peter", "Lustig");
- */
-Person_t p_1 = {.arrival = 'Z', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL, NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
-Person_t p_2 = {.arrival = 'R', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL, NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
-Person_t p_3 = {.arrival = 'O', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL, NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
+
+}
+Person_t p_1 = {.arrival = 'Z', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL,
+                                                                                             NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
+Person_t p_2 = {.arrival = 'R', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL,
+                                                                                             NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
+Person_t p_3 = {.arrival = 'O', .first_name = "Stefan", .last_name = "Hubolt", .neighbour = {NULL,
+                                                                                             NULL}, .seat = NULL, .node = {.next = NULL, .prev = NULL}};
 
 /*
 TEST(seat_rows_unit_tests, createRow)
@@ -27,23 +23,33 @@ TEST(seat_rows_unit_tests, createRow)
 EXPECT_EQ(createRow(), NULL);
 }
 */
-TEST(seat_rows_unit_tests, addPersontoRow)
+TEST(seat_rows_unit_tests, addPersontoRow )
 {
 
-    ListRows_t *row1 = createRow();
-    occupySeat(row1, &p_1);
-    Seat_t *current = rows[i - 1 ]->start; //why rows[i -1 ] ??
-    if ( current->occupied != NULL) {
-        printf("Name: %s %s \n", current->occupied->first_name, current->occupied->last_name );
-    } else {
-        printf("<NULL>\n");
-    }
+ListRows_t *row1 = createRow();
+ListRows_t *row2 = createRow();
+
+
+ListRows_t *rows[2] = {row1, row2};
+
+occupySeat(row1, &p_1 ) ;
+Seat_t *current = rows[0]->start; //Seat_t *current = rows[ i - 1 ]->start;
+if ( current -> occupied != NULL ) {
+printf ( "Name: %s %s \n" , current -> occupied -> first_name , current -> occupied -> last_name ) ;
+} else {
+printf("<NULL>\n");
+}
+
 
 }
 
 
+
 // This is a dummy-test, which will always be passed. It serves as a template for other testcases
+/*
 TEST(seat_rows_unit_tests, DUMMY)
 {
 	EXPECT_EQ(1, 1);
 }
+*/
+
