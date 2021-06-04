910b5e1c3ca84b8da0c263015428c0c2d2891230
Stefan <stefan.bittgen@gmx.net>
2021-06-04 22:38:20 +0200

    fixing bugs

--- tests/seat_row_tests.cxx
+++ tests/seat_row_tests.cxx
@@ -19,6 +19,7 @@ Person_t p_9 = {.arrival = 'R', .first_name = "Fillin", .last_name = "Kasper", .
 Person_t p_10 = {.arrival = 'R', .first_name = "Afua", .last_name = "Mara", .neighbour = {nullptr, nullptr}, .seat = nullptr, .node = {.next = nullptr, .prev = nullptr}};
 
 ListRows_t *row1 = createRow();
+
 ListRows_t *row2 = createRow();
 ListRows_t *row3 = createRow();
 ListRows_t *row4 = createRow();
@@ -26,15 +27,7 @@ ListRows_t *row5 = createRow();
 
 ListRows_t *rows[5] = {row1, row2, row3, row4, row5};
 
-// occupySeat(row1, &p_1);
-// occupySeat(row2, &p_2);
-// occupySeat(row3, &p_3);
-// occupySeat(row2, &p_4);
-// occupySeat(row1, &p_5);
-// occupySeat(row4, &p_6);
-// occupySeat(row5, &p_7);
-// occupySeat(row1, &p_8);
-// occupySeat(row1, &_9);
+
 
 // This is a dummy-test, which will always be passed. It serves as a template for other testcases
 TEST(seat_rows_unit_tests, DUMMY)
@@ -68,24 +61,6 @@ TEST(seat_rows_unit_tests, occupySeats_t2)
 	EXPECT_EQ(occupySeat(rows[1], &p_2), 0);
 }
 
-// 0 is expected since occupying was successful
-TEST(seat_rows_unit_tests, occupySeats_t4)
-{
-	occupySeat(rows[1], &p_4);
-	EXPECT_EQ(occupySeat(rows[1], &p_4), 0);
-}
-// 0 is expected since occupying was successful
-TEST(seat_rows_unit_tests, occupySeats_t5)
-{
-	occupySeat(rows[1], &p_5);
-	EXPECT_EQ(occupySeat(rows[1], &p_5), 0);
-}
-// Not 0 is expected since occupying must not be successful
-TEST(seat_rows_unit_tests, occupySeats_t6)
-{
-	occupySeat(rows[1], &p_6);
-	EXPECT_NE(occupySeat(rows[1], &p_6), 0);
-}
 // 0 is expected for select row to be successful
 TEST(seat_rows_unit_tests, selectRow_t1)
 {
