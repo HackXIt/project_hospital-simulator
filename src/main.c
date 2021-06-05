d2a8a80d2a5186aa00246c9272ad34ad4592e791
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-06-05 15:19:21 +0200

    Added help-message, commented out `-b` feature in options

--- src/main.c
+++ src/main.c
@@ -22,7 +22,6 @@ int main(int argc, char *argv[])
 	if (argc == 1) // Program is executed normally in GUI-Mode (no parameters given)
 	{
 		// NOTE GTK-Main-Function is executed here
-		// TODO Datenspeicher initialisieren
 		ListPersons_t *persons_active = createListPersons();
 		ListPersons_t *persons_completed = createListPersons();
 		ListRows_t *row_1 = createRow();
@@ -31,7 +30,6 @@ int main(int argc, char *argv[])
 		ListRows_t *row_4 = createRow();
 		ListRows_t *row_5 = createRow();
 		ListRows_t *rows[MAX_ROWS] = {row_1, row_2, row_3, row_4, row_5};
-		// gtk_main(persons_active, persons_completed, rows);
 		gui_main(argc, argv, persons_active, persons_completed, rows);
 		// FIXME Shouldn't we export the active-list as well? To document the persons currently still sitting?
 		exportListPersons(persons_completed);
@@ -45,33 +43,34 @@ int main(int argc, char *argv[])
 	else if (argc > 1) // Program is executed in CLI-Mode (parameters exist)
 	{
 		int opt;
-		char *valid_options = "b:h"; // NOTE Please retain the order of the options alphabetically
+		char *valid_options = "h"; // NOTE Please retain the order of the options alphabetically
 		/* NOTE Valid options for CLI-Execution:
-		b = binary | Read from provided binary to export it into output-file
+		b = binary | Read from provided binary/tmp-storage to export it into output-file
 		h = help | Print help message for program execution
 		*/
-		bool b_given = false;
+		// bool b_given = false;
 		while ((opt = getopt(argc, argv, valid_options))) // Parse parameters
 		{
 			switch (opt) // NOTE Please retain the order of this switch-statement alphabetically
 			{
-			case 'b':
-				// TODO Parse argument of b
-				b_given = true;
-				break;
+			// case 'b':
+			// 	// TODO Parse argument of b
+			// 	b_given = true;
+			// 	break;
 			case 'h':
 				print_help(argv[0]);
 				break;
 			default:
 				fprintf(stderr, "Non-valid option given.\n");
+				print_help(argv[0]);
 				exit(EXIT_FAILURE);
 				break;
 			}
 		}
-		if (b_given)
-		{
-			fprintf(stderr, "Not implemented.\n");
-		}
+		// if (b_given)
+		// {
+		// 	fprintf(stderr, "Not implemented.\n");
+		// }
 	}
 
 	// Possible example of Person-List-creation: ListPersons_t activePersons = createListPersons();
@@ -85,6 +84,10 @@ int main(int argc, char *argv[])
 void print_help(char *const argzero)
 {
 	// TODO proper help message, currently just placeholer
-	printf("%s [OPTIONS]... [WORDS]...\n", argzero);
-	printf("Hello World!\n");
+	printf("%s [OPTIONS]... [ARGUMENTS]...\n", argzero);
+	printf("Application to track patients & their seat-neighbours in a simulated hospital.\n\n");
+	printf("When no options are given, program is executed normally with a Graphical-User-Interface.\n");
+	printf("When options are given, program is executed in CLI-Mode, providing optional features.\n\n");
+	printf("----OPTIONS:\n");
+	printf("\t -h \t\t print this help text.\n");
 }
