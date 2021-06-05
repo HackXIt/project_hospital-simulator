653027541d3c234231bfc7c5a9d1201559a18388
Nikolaus Rieder <ic20b028@technikum-wien.atgit config --local user.name ic20b028git config --local user.email ic20b028@technikum-wien.at>
2021-06-05 14:50:52 +0200

    Wrote quick-save/load functions but still having errors

--- inc/quick_save.h
+++ inc/quick_save.h
@@ -1,10 +1,14 @@
 #ifndef QUICK_SAVE_H
 #define QUICK_SAVE_H
+
+#define NUM_LINES 8
 typedef struct hospital_translators
 {
 	ser_tra_t *t_node_p;
 	ser_tra_t *t_person;
-	ser_tra_t *t_person_ref;
+	ser_tra_t *t_neighbour;
+	// ser_tra_t *t_person_ref;
 } hospital_tra_t;
-void quick_save(ListPersons_t *active, ListPersons_t *completed);
+void quick_save(ListPersons_t *active, ListPersons_t *completed, hospital_tra_t *translators);
+ListPersons_t *quick_load(hospital_tra_t *translators, FILE *stream);
 #endif
--- src/quick_save.c
+++ src/quick_save.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hospital_structures.h"
 #include "serialize.h" // Custom Library from the Internet to serialize C-Structures (Credits/Sources in serialize.h)
@@ -7,9 +8,71 @@
 #include "persons.h"
 #include "quick_save.h"
 
-void quick_save(ListPersons_t *active, ListPersons_t *completed)
+void quick_save(ListPersons_t *active, ListPersons_t *completed, hospital_tra_t *translators)
 {
-	return;
+	FILE *tmp;
+	char *output;
+
+	// Serializing active-list
+	tmp = fopen("active_persons.tmp", "w");
+	output = ser_ialize(translators->t_person, "person", active->start, NULL, 0);
+	fprintf(tmp, "%s", output);
+	fflush(tmp);
+	fclose(tmp);
+	// Serializing completed-list
+	free(output);
+	tmp = fopen("completed_persons.tmp", "w");
+	output = ser_ialize(translators->t_person, "person", completed->start, NULL, 0);
+	fprintf(tmp, "%s", output);
+	fflush(tmp);
+	fclose(tmp);
+	free(output);
+}
+
+ListPersons_t *quick_load(hospital_tra_t *translators, FILE *stream)
+{
+	char *line = NULL;
+	char *input = strdup("\0");
+	size_t len = 0;
+	ssize_t read = 1;
+	Person_t *tmp = NULL;
+	// size_t end = 0;
+
+	while ((read = getline(&line, &len, stream) != -1))
+	{
+		// Concatenate line to input string
+		// end = strlen(line) + 1;
+		// in_end = strlen(input) + 1;
+		input = (char *)realloc(input, (strlen(input) + 1 + len + 1));
+		strcat(input, line);
+		// Since we got 1x line, another 8 must follow
+		for (int i = 0; i < NUM_LINES - 1; i++)
+		{
+			getline(&line, &len, stream);
+			input = (char *)realloc(input, (strlen(input) + 1 + len + 1));
+			strcat(input, line);
+		}
+	}
+	// Remove unwanted newlines
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		if (input[i] == '\n')
+		{
+			input[i] = ' ';
+		}
+	}
+	tmp = (Person_t *)ser_parse(translators->t_person, "person", input, NULL);
+	Person_t *start = tmp;
+	printf("%p\n", (void *)start);
+	while (tmp != NULL)
+	{
+		printf("%p\n", (void *)tmp);
+		printPerson(tmp);
+		tmp = tmp->node.next;
+		free(tmp->node.prev);
+	}
+	free(input);
+	return NULL;
 }
 
 hospital_tra_t *initialize_translators()
@@ -24,20 +87,23 @@ hospital_tra_t *initialize_translators()
 	ser_new_field(translators->t_node_p, "person", 1, "p_next", offsetof(struct node_p, next));
 	ser_new_field(translators->t_node_p, "person", 1, "p_prev", offsetof(struct node_p, prev));
 	ser_new_field(translators->t_person, "node_p", 0, "node_p", offsetof(Person_t, node));
-	translators->t_person_ref = ser_new_tra("person_ref", sizeof(Person_t), translators->t_person);
-	ser_new_field(translators->t_person_ref, "string", 0, "first_name", offsetof(Person_t, first_name));
-	ser_new_field(translators->t_person_ref, "string", 0, "last_name", offsetof(Person_t, last_name));
-	ser_new_field(translators->t_person, "person_ref", 1, "left_neighbour", offsetof(Person_t, neighbour[0]));
-	ser_new_field(translators->t_person, "person_ref", 1, "right_neighbour", offsetof(Person_t, neighbour[1]));
+	translators->t_neighbour = ser_new_nullterm_array("neighbours", "person", 1, translators->t_person);
+	ser_new_field(translators->t_person, "neighbours", 0, "neighbors", offsetof(Person_t, neighbour));
+	translators->t_neighbour->first_field->repeat = 2;
+	// translators->t_person_ref = ser_new_tra("person_ref", sizeof(Person_t), translators->t_person);
+	// ser_new_field(translators->t_person_ref, "char", 0, "arrival", offsetof(Person_t, arrival));
+	// ser_new_field(translators->t_person_ref, "string", 0, "first_name", offsetof(Person_t, first_name));
+	// ser_new_field(translators->t_person_ref, "string", 0, "last_name", offsetof(Person_t, last_name));
+	// ser_new_field(translators->t_person, "person_ref", 1, "left_neighbour", offsetof(Person_t, neighbour[0]));
+	// ser_new_field(translators->t_person, "person_ref", 1, "right_neighbour", offsetof(Person_t, neighbour[1]));
 	return translators;
 }
 
 /* NOTE This main is for testing-purposes of quick_save.c and can be compiled using `make quicksave`
+*/
 int main(int argc, char const *argv[])
 {
 	hospital_tra_t *trans = initialize_translators();
-	out = fopen("test.tmp", "w");
-	char *output;
 	ListPersons_t *p_active = createListPersons();
 	ListPersons_t *p_completed = createListPersons();
 	ListRows_t *row_1 = createRow();
@@ -82,15 +148,16 @@ int main(int argc, char const *argv[])
 	selectRow(rows, p11);
 	addPerson(p_active, p12);
 	selectRow(rows, p12);
-	// quick_save(p_active, p_completed, rows);
+	quick_save(p_active, p_completed, trans);
+	// quick_load(trans, fopen("active_persons.tmp", "r"));
 	// output = ser_ialize(trans->t_person, "person", p_active->start, NULL, 0);
-	output = ser_ialize(trans->t_person, "person", p_active->start, NULL, 0);
-	fprintf(out, "Result:\n%s\n", output);
-	printf("Result:\n%s\n", output);
-	free(output);
-	// fflush(out);
-	// fclose(out);
-	printListPersons(p_active);
+	// output = ser_ialize(trans->t_person, "person", p_active->start, NULL, 0);
+	// fprintf(out, "Result:\n%s\n", output);
+	// printf("Result:\n%s\n", output);
+	// free(output);
+	// // fflush(out);
+	// // fclose(out);
+	// printListPersons(p_active);
 
 	// out = fopen("test2.dat", "wb");
 
@@ -121,11 +188,14 @@ int main(int argc, char const *argv[])
 	clearSeat(p_completed->last);
 	movePerson(p_active, p_completed);
 	clearSeat(p_completed->last);
-	// quick_save(p_active, p_completed, rows);
-	output = ser_ialize(trans->t_person, "person", p_completed->start, NULL, 0);
-	printf("Result:\n%s\n", output);
-	free(output);
-	printListPersons(p_completed);
+	quick_save(p_active, p_completed, trans);
+
+	printf("\n\n----BREAK----\n\n");
+	quick_load(trans, fopen("completed_persons.tmp", "r"));
+	// output = ser_ialize(trans->t_person, "person", p_completed->start, NULL, 0);
+	// printf("Result:\n%s\n", output);
+	// free(output);
+	// printListPersons(p_completed);
 	// exportListPersons(p_completed);
 
 	// fflush(out);
@@ -146,4 +216,5 @@ int main(int argc, char const *argv[])
 	free(trans);
 	return 0;
 }
+/*
 */
